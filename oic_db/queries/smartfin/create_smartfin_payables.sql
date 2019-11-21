f4a164bb099df6fa37e96298cbe7c90381c1dea7
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 18:24:59 -0300

    Update create_smartfin_payables.sql
    - Adicionados campos erp_receivable_id e erp_clustered_receivable_id para serem gravados na payables para facilitar a rastreabilidade;
    - Correção da query, pois o relacionamento inicial estava incorreto;

--- oic_db/queries/smartfin/create_smartfin_payables.sql
+++ oic_db/queries/smartfin/create_smartfin_payables.sql
@@ -16,36 +16,36 @@ declare v_message_text varchar(255);
 declare v_conciliator_id varchar(45);
 declare v_keycontrol varchar(150);
 declare v_id_receivable int;
+declare v_erp_receivable_id int;
+declare v_erp_clustered_receivable_id int;
 declare v_id_receivable_smartfin int;
 declare done int;
 declare cur1 cursor for 
-
 					select
-						rec.id
-                        ,receivable_id_smartfin
+						 rec.id
+						,rec.erp_receivable_id
+                        ,rec.erp_clustered_receivable_id
+						,receivable_id_smartfin
+                        ,otc.erp_business_unit
+                        ,otc.erp_legal_entity
+                        ,otc.erp_subsidiary
+                        ,otc.acronym
 					from receivable rec
 					
-					inner join order_to_cash
-					on order_to_cash.id = rec.order_to_cash_id
+					inner join order_to_cash otc
+					on otc.id = rec.order_to_cash_id
 					
-					inner join clustered_receivable_customer crc
-					on crc.identification_financial_responsible = order_to_cash.erp_receivable_customer_identification
-					and crc.is_smartfin = 'yes' -- Neste caso como haverá uma integração separada para os movimentos Smartfin esse filtro deverá ser fixo para tais movimentos
-                    
-					inner join organization_from_to_version oftv
-					on oftv.erp_legal_entity = crc.identification_financial_responsible 					
-					
-					where order_to_cash.country = p_country
-					and order_to_cash.origin_system = p_origin_system
-					and order_to_cash.operation = p_operation
-					and order_to_cash.erp_receivable_status_transaction = 'created_at_erp' -- Filtrar somente os registros que já foram integrados o AR no Oracle
-					and order_to_cash.erp_receipt_status_transaction = 'created_at_erp' -- Filtrar somente os registros que já foram baixados (Receipt)
+					where otc.country = p_country
+					and otc.origin_system = p_origin_system
+					and otc.operation = p_operation
+					and rec.transaction_type = p_transaction_type -- Integração em paralalo por tipo de transação (cartão de crédito recorrente, cartão de débito recorrente, débito em conta, boleto etc...)
+					and otc.erp_receivable_status_transaction = 'created_at_erp' -- Filtrar somente os registros que já foram integrados o AR no Oracle
+					and otc.erp_receipt_status_transaction = 'created_at_erp' -- Filtrar somente os registros que já foram baixados (Receipt)
 					and rec.erp_clustered_receivable_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable
 					and rec.erp_clustered_receivable_customer_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable_customer 
 					and rec.erp_receivable_id is not null -- Filtrar somente os receivables que já foram integrados com o erp
-					and rec.erp_receivable_receipt_id is not null -- Filtrar somente os receivables que já foram integrados com o erp
-					and rec.transaction_type = p_transaction_type ;-- Integração em paralalo por tipo de transação (cartão de crédito recorrente, cartão de débito recorrente, débito em conta, boleto etc...)
-
+					and rec.erp_receivable_receipt_id is not null -- Filtrar somente os receivables que já foram integrados com o erp					
+					and rec.receivable_id_smartfin is not null; -- Filtrar somente os registros de smartfin que foram gerados através da proc sp_create_smartfin_receivables;
 
 declare continue handler for not found set done=1;
 
@@ -62,45 +62,42 @@ if get_lock(@v_keycontrol,1) = 1 and  exists ( select 1 from clustered_receivabl
 									  inner join organization_from_to_version oftv
 									  on oftv.erp_legal_entity = crc.identification_financial_responsible 
 									  where is_smartfin = 'yes')  then 
-
+	    
+    
     set done = 0;	
     open cur1;
     
     SmartfinPayablesLoop: loop
     
-        fetch cur1 into v_id_receivable, v_id_receivable_smartfin;
-                        
-		if done = 1 then leave SmartfinPayablesLoop; end if;
-
+        fetch cur1 into v_id_receivable, v_erp_receivable_id, v_erp_clustered_receivable_id, v_id_receivable_smartfin, v_erp_business_unit, v_erp_legal_entity, v_erp_subsidiary, v_acronym;
+		
+        if done = 1 then leave SmartfinPayablesLoop; end if;			
+	
 		start transaction;
 		
         if exists (
 					select 
-						 @v_unity_identification := oftv.organization_from_to_unity_identification 
-						,@v_erp_business_unit := oftv.erp_business_unit 
-						,@v_erp_legal_entity := oftv.erp_legal_entity
-						,@v_erp_subsidiary := oftv.erp_subsidiary
-						,@v_acronym := oftv.acronym
-						,@v_erp_supplier_id := sup.erp_supplier_id
+						 @v_erp_supplier_id := sup.erp_supplier_id
 						,@v_erp_payable_supplier_identification := sup.identification_financial_responsible
 					from receivable rec
 					
 					inner join order_to_cash otc
 					on otc.id = rec.order_to_cash_id
-					
-					inner join clustered_receivable_customer crc
-					on crc.identification_financial_responsible = otc.erp_receivable_customer_identification
-					
+									
 					inner join organization_from_to_version oftv
-					on oftv.erp_legal_entity = crc.identification_financial_responsible 
+					on oftv.erp_business_unit = otc.erp_business_unit
+                    and oftv.erp_legal_entity = otc.erp_legal_entity
+                    and oftv.erp_subsidiary = otc.erp_subsidiary
 					and oftv.created_at = ( select 
 												max(oftv2.created_at) 
 											from organization_from_to_version oftv2 
-											where oftv2.organization_from_to_unity_identification = oftv.organization_from_to_unity_identification 
+											where oftv2.erp_business_unit = oftv.erp_business_unit 
+                                            and oftv2.erp_legal_entity = oftv.erp_legal_entity 
+                                            and oftv2.erp_subsidiary = oftv.erp_subsidiary
 											)
 
 					inner join supplier sup
-                    on sup.identification_financial_responsible = crc.identification_financial_responsible
+                    on sup.identification_financial_responsible = oftv.fiscal_federal_identification
                     and sup.erp_supplier_id is not null
                     
 					where rec.id = v_id_receivable_smartfin ) and
@@ -125,6 +122,8 @@ if get_lock(@v_keycontrol,1) = 1 and  exists ( select 1 from clustered_receivabl
 				acronym,
 				erp_payable_id,
 				receivable_id,
+                erp_receivable_id,
+                erp_clustered_receivable_id,
 				erp_supplier_id,
 				issue_date,
 				due_date,
@@ -132,15 +131,21 @@ if get_lock(@v_keycontrol,1) = 1 and  exists ( select 1 from clustered_receivabl
 				erp_payable_returned_from_erp_at,
 				erp_payable_status_transaction,
 				erp_payable_log,
+				erp_receipt_send_to_erp_at,
+				erp_receipt_returned_from_erp_at,
+				erp_receipt_status_transaction,
+				erp_receipt_log,                
 				erp_filename,
 				erp_line_in_file)
 				VALUES
-				(@v_erp_business_unit, -- erp_business_unit
-				@v_erp_legal_entity, -- erp_legal_entity
-				@v_erp_subsidiary, -- erp_subsidiary
-				@v_acronym, -- acronym
+				(v_erp_business_unit, -- erp_business_unit
+				v_erp_legal_entity, -- erp_legal_entity
+				v_erp_subsidiary, -- erp_subsidiary
+				v_acronym, -- acronym
 				null, -- erp_payable_id
 				v_id_receivable, -- receivable_id
+                v_erp_receivable_id, -- erp_receivalbe_id
+                v_erp_clustered_receivable_id, -- erp_clustered_receivable_id
 				@v_erp_supplier_id, -- erp_supplier_id
 				current_date(), -- issue_date
 				current_date(), -- due_date
@@ -148,10 +153,13 @@ if get_lock(@v_keycontrol,1) = 1 and  exists ( select 1 from clustered_receivabl
 				null, -- erp_payable_returned_from_erp_at
 				'waiting_to_be_process', -- erp_payable_status_transaction
 				null, -- erp_payable_log
+				null, -- erp_receipt_send_to_erp_at
+				null, -- erp_receipt_returned_from_erp_at
+				'waiting_to_be_process', -- erp_receipt_status_transaction
+				null, -- erp_receipt_log                
 				null, -- erp_filename
 				null); -- erp_line_in_file
 			
-            
             update receivable 
             
             inner join order_to_cash
