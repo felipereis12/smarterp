5568aac98a44c8c05cf98d2abb938a683c21ca93
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 17:09:10 -0300

    Delete calling_procedure_create_smartfin_operation.sql
    O nome desse script foi alterado para calling_procedure_create_smartfin_receivables

--- oic_db/queries/smartfin/calling_procedure_create_smartfin_operation.sql
+++
@@ -1,3 +0,0 @@
-set sql_mode = traditional;
-call sp_create_smartfin_operation('Brazil','smartsystem','person_plan','credit_card_recurring');
-call sp_create_smartfin_operation('Brazil','smartsystem','person_plan','debit_account_recurring');
