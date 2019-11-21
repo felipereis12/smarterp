a8a9d2c780ab7cba23667b37f651b5bbcd44daaa
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 15:11:00 -0300

    Update select.sql
    Script de teste

--- oic_db/queries/various/select.sql
+++ oic_db/queries/various/select.sql
@@ -4,7 +4,6 @@ select * from receivable;
 select * from clustered_receivable;
 -- select * from refund;
 -- select * from refund_items;
-
 -- select * from clustered_receivable_customer;
 -- select * from invoice;
 -- select * from invoice_items;
