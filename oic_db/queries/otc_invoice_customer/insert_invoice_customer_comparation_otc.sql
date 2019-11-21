2028c25802dcdb2de708f836c542ad386c36410b
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 14:53:55 -0300

    Update insert_invoice_customer_comparation_otc.sql
    Criação de alias para facilitar o entendimento e diminuir o tamanho da query.

--- oic_db/queries/otc_invoice_customer/insert_invoice_customer_comparation_otc.sql
+++ oic_db/queries/otc_invoice_customer/insert_invoice_customer_comparation_otc.sql
@@ -24,57 +24,57 @@ insert into invoice_customer_comparation (
                 erp_line_in_file )
                 
 select
-        invoice_customer.order_to_cash_id,
-        invoice_customer.country,                
-        invoice_customer.erp_customer_id,
-        invoice_customer.full_name,
-        invoice_customer.type_person,
-        invoice_customer.identification_financial_responsible,
-        invoice_customer.nationality_code,
-        invoice_customer.state,
-        invoice_customer.city,
-        invoice_customer.adress,
-        invoice_customer.adress_number,
-        invoice_customer.adress_complement,
-        invoice_customer.district,
-        invoice_customer.postal_code,
-        invoice_customer.area_code,
-        invoice_customer.cellphone,
-        invoice_customer.email,
-        invoice_customer.state_registration,
-        invoice_customer.federal_registration,
-        invoice_customer.final_consumer,
-        invoice_customer.icms_contributor,
-        invoice_customer.erp_filename,
-        invoice_customer.erp_line_in_file
-from invoice_customer 
+        ivc.order_to_cash_id,
+        ivc.country,                
+        ivc.erp_customer_id,
+        ivc.full_name,
+        ivc.type_person,
+        ivc.identification_financial_responsible,
+        ivc.nationality_code,
+        ivc.state,
+        ivc.city,
+        ivc.adress,
+        ivc.adress_number,
+        ivc.adress_complement,
+        ivc.district,
+        ivc.postal_code,
+        ivc.area_code,
+        ivc.cellphone,
+        ivc.email,
+        ivc.state_registration,
+        ivc.federal_registration,
+        ivc.final_consumer,
+        ivc.icms_contributor,
+        ivc.erp_filename,
+        ivc.erp_line_in_file
+from invoice_customer ivc
 
-inner join order_to_cash
-on order_to_cash.id = invoice_customer.order_to_cash_id
+inner join order_to_cash otc
+on otc.id = ivc.order_to_cash_id
 
-where order_to_cash.country = 'Brazil' -- O filtro para encontrar o registro na invoice_customer deve ser composto pelo país + identificação cpf, cnpj ou passaporte (estrangeiros)
-and invoice_customer.identification_financial_responsible = '39367233892' -- no caso do Brasil esse canterá cpf para pessoas físicas, cnpj para pessoas jurídicas e passaporte para estrangeiros
+where otc.country = 'Brazil' -- O filtro para encontrar o registro na invoice_customer deve ser composto pelo país + identificação cpf, cnpj ou passaporte (estrangeiros)
+and ivc.identification_financial_responsible = '39367233892' -- no caso do Brasil esse canterá cpf para pessoas físicas, cnpj para pessoas jurídicas e passaporte para estrangeiros
 
 on duplicate key 
 
-update invoice_customer_comparation.order_to_cash_id = invoice_customer.order_to_cash_id,
-invoice_customer_comparation.erp_customer_id = invoice_customer.erp_customer_id,
-invoice_customer_comparation.full_name = invoice_customer.full_name,
-invoice_customer_comparation.type_person = invoice_customer.type_person,
-invoice_customer_comparation.nationality_code = invoice_customer.nationality_code,
-invoice_customer_comparation.state = invoice_customer.state,
-invoice_customer_comparation.city = invoice_customer.city,
-invoice_customer_comparation.adress = invoice_customer.adress,
-invoice_customer_comparation.adress_number = invoice_customer.adress_number,
-invoice_customer_comparation.adress_complement = invoice_customer.adress_complement,
-invoice_customer_comparation.district = invoice_customer.district,
-invoice_customer_comparation.postal_code = invoice_customer.postal_code,
-invoice_customer_comparation.area_code = invoice_customer.area_code,
-invoice_customer_comparation.cellphone = invoice_customer.cellphone,
-invoice_customer_comparation.email = invoice_customer.email,
-invoice_customer_comparation.state_registration = invoice_customer.state_registration,
-invoice_customer_comparation.federal_registration = invoice_customer.federal_registration,
-invoice_customer_comparation.final_consumer = invoice_customer.final_consumer,
-invoice_customer_comparation.icms_contributor = invoice_customer.icms_contributor,
-invoice_customer_comparation.erp_filename = invoice_customer.erp_filename,
-invoice_customer_comparation.erp_line_in_file = invoice_customer.erp_line_in_file
+update invoice_customer_comparation.order_to_cash_id = ivc.order_to_cash_id,
+invoice_customer_comparation.erp_customer_id = ivc.erp_customer_id,
+invoice_customer_comparation.full_name = ivc.full_name,
+invoice_customer_comparation.type_person = ivc.type_person,
+invoice_customer_comparation.nationality_code = ivc.nationality_code,
+invoice_customer_comparation.state = ivc.state,
+invoice_customer_comparation.city = ivc.city,
+invoice_customer_comparation.adress = ivc.adress,
+invoice_customer_comparation.adress_number = ivc.adress_number,
+invoice_customer_comparation.adress_complement = ivc.adress_complement,
+invoice_customer_comparation.district = ivc.district,
+invoice_customer_comparation.postal_code = ivc.postal_code,
+invoice_customer_comparation.area_code = ivc.area_code,
+invoice_customer_comparation.cellphone = ivc.cellphone,
+invoice_customer_comparation.email = ivc.email,
+invoice_customer_comparation.state_registration = ivc.state_registration,
+invoice_customer_comparation.federal_registration = ivc.federal_registration,
+invoice_customer_comparation.final_consumer = ivc.final_consumer,
+invoice_customer_comparation.icms_contributor = ivc.icms_contributor,
+invoice_customer_comparation.erp_filename = ivc.erp_filename,
+invoice_customer_comparation.erp_line_in_file = ivc.erp_line_in_file
