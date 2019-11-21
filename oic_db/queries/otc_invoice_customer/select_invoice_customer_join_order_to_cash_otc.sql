68ad48cfbdd5c33ecb631eeafa2b70c92318a839
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 14:54:34 -0300

    Update select_invoice_customer_join_order_to_cash_otc.sql
    Utilização de alias em tabelas para facilitar o entendimento e diminuir o tamanho da query.

--- oic_db/queries/otc_invoice_customer/select_invoice_customer_join_order_to_cash_otc.sql
+++ oic_db/queries/otc_invoice_customer/select_invoice_customer_join_order_to_cash_otc.sql
@@ -1,82 +1,82 @@
 select
-		order_to_cash.origin_system,
-		invoice_customer.identification_financial_responsible,
-		invoice_customer.full_name,
-		invoice_customer.adress,
-		invoice_customer.adress_number,
-		invoice_customer.adress_complement,
-		invoice_customer.district,
-		invoice_customer.city,
-		invoice_customer.state,
-		invoice_customer.postal_code,
-		invoice_customer.nationality_code,
-		invoice_customer.area_code,
-		invoice_customer.cellphone,
-		invoice_customer.email,
-		invoice_customer.erp_customer_id
-from order_to_cash 
+		otc.origin_system,
+		ivc.identification_financial_responsible,
+		ivc.full_name,
+		ivc.adress,
+		ivc.adress_number,
+		ivc.adress_complement,
+		ivc.district,
+		ivc.city,
+		ivc.state,
+		ivc.postal_code,
+		ivc.nationality_code,
+		ivc.area_code,
+		ivc.cellphone,
+		ivc.email,
+		ivc.erp_customer_id
+from order_to_cash otc
 
-inner join invoice_customer  
-on order_to_cash.id = invoice_customer.order_to_cash_id
+inner join invoice_customer ivc
+on otc.id = ivc.order_to_cash_id
 
-inner join invoice_customer_comparation  
-on invoice_customer.erp_customer_id = invoice_customer_comparation.erp_customer_id 
-and invoice_customer.identification_financial_responsible = invoice_customer_comparation.identification_financial_responsible
+inner join invoice_customer_comparation ivcc 
+on ivc.erp_customer_id = ivcc.erp_customer_id 
+and ivc.identification_financial_responsible = ivcc.identification_financial_responsible
 
-where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
-and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
-and order_to_cash.origin_system = 'smartsystem' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'person_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
-and order_to_cash.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and order_to_cash.to_generate_customer = 'yes'
+where otc.country = 'Brazil' -- Integração em paralelo por operação do país
+and otc.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
+and otc.origin_system = 'smartsystem' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
+and otc.operation = 'person_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
+and otc.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
+and otc.to_generate_customer = 'yes'
 and (
-	invoice_customer.erp_customer_id <> invoice_customer_comparation.erp_customer_id
-	or invoice_customer.full_name <> invoice_customer_comparation.full_name
-	or invoice_customer.type_person <> invoice_customer_comparation.type_person
-	or invoice_customer.nationality_code <> invoice_customer_comparation.nationality_code
-	or invoice_customer.state <> invoice_customer_comparation.state
-	or invoice_customer.city <> invoice_customer_comparation.city
-	or invoice_customer.adress <> invoice_customer_comparation.adress
-	or invoice_customer.adress_number <> invoice_customer_comparation.adress_number
-	or invoice_customer.adress_complement <> invoice_customer_comparation.adress_complement 
-	or invoice_customer.district <> invoice_customer_comparation.district
-	or invoice_customer.postal_code <> invoice_customer_comparation.postal_code
-	or invoice_customer.area_code <> invoice_customer_comparation.area_code
-	or invoice_customer.cellphone <> invoice_customer_comparation.cellphone
-	or invoice_customer.email <> invoice_customer_comparation.email
-	or invoice_customer.state_registration <> invoice_customer_comparation.state_registration
-	or invoice_customer.federal_registration <> invoice_customer_comparation.federal_registration
-	or invoice_customer.final_consumer <> invoice_customer_comparation.final_consumer
-	or invoice_customer.icms_contributor <> invoice_customer_comparation.icms_contributor
+	   ivc.erp_customer_id <> ivcc.erp_customer_id
+	or ivc.full_name <> ivcc.full_name
+	or ivc.type_person <> ivcc.type_person
+	or ivc.nationality_code <> ivcc.nationality_code
+	or ivc.state <> ivcc.state
+	or ivc.city <> ivcc.city
+	or ivc.adress <> ivcc.adress
+	or ivc.adress_number <> ivcc.adress_number
+	or ivc.adress_complement <> ivcc.adress_complement 
+	or ivc.district <> ivcc.district
+	or ivc.postal_code <> ivcc.postal_code
+	or ivc.area_code <> ivcc.area_code
+	or ivc.cellphone <> ivcc.cellphone
+	or ivc.email <> ivcc.email
+	or ivc.state_registration <> ivcc.state_registration
+	or ivc.federal_registration <> ivcc.federal_registration
+	or ivc.final_consumer <> ivcc.final_consumer
+	or ivc.icms_contributor <> ivcc.icms_contributor
 )
                 
 union
 
 select
-		order_to_cash.origin_system,
-		invoice_customer.identification_financial_responsible,
-		invoice_customer.full_name,
-		invoice_customer.adress,
-		invoice_customer.adress_number,
-		invoice_customer.adress_complement,
-		invoice_customer.district,
-		invoice_customer.city,
-		invoice_customer.state,
-		invoice_customer.postal_code,
-		invoice_customer.nationality_code,
-		invoice_customer.area_code,
-		invoice_customer.cellphone,
-		invoice_customer.email,
-		invoice_customer.erp_customer_id
-from order_to_cash order_to_cash
+		otc.origin_system,
+		ivc.identification_financial_responsible,
+		ivc.full_name,
+		ivc.adress,
+		ivc.adress_number,
+		ivc.adress_complement,
+		ivc.district,
+		ivc.city,
+		ivc.state,
+		ivc.postal_code,
+		ivc.nationality_code,
+		ivc.area_code,
+		ivc.cellphone,
+		ivc.email,
+		ivc.erp_customer_id
+from order_to_cash otc
 
-inner join invoice_customer 
-on order_to_cash.id = invoice_customer.order_to_cash_id
+inner join invoice_customer ivc
+on otc.id = ivc.order_to_cash_id
 
-where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
-and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
-and order_to_cash.origin_system = 'smartsystem' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'person_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
-and order_to_cash.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and order_to_cash.to_generate_customer = 'yes'
-and invoice_customer.erp_customer_id is null;
+where otc.country = 'Brazil' -- Integração em paralelo por operação do país
+and otc.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
+and otc.origin_system = 'smartsystem' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
+and otc.operation = 'person_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
+and otc.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
+and otc.to_generate_customer = 'yes'
+and ivc.erp_customer_id is null;
