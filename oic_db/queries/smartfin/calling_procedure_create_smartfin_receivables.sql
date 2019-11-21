20af20cefb9e556799f788d572136f7da397fff1
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 18:22:06 -0300

    Create calling_procedure_create_smartfin_receivables.sql
    Script para chamada de teste da procedure sp_create_smartfin_receivables

---
+++ oic_db/queries/smartfin/calling_procedure_create_smartfin_receivables.sql
@@ -0,0 +1,3 @@
+set sql_mode = traditional;
+call sp_create_smartfin_receivables('Brazil','smartsystem','person_plan','credit_card_recurring');
+call sp_create_smartfin_receivables('Brazil','smartsystem','person_plan','debit_account_recurring');
