482359e8c36c7339265903d98a33194de1b86662
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-21 18:18:45 -0300

    Update select_chargeback.sql
    Adicionado join com a tabela clustered_chargeback, pois nela há em qual business unit o movimento deverá ser gerado;
    
    Adicionada condição do campo receivables.converted_smartfin para desconsiderar os movimentos alterados pela procedure sp_create_smartfin_receivables;
    
    Adicionado alias's em tabelas para facilitar o entendimento e diminuir o tamanho da query;

--- oic_db/queries/chargeback/select_chargeback.sql
+++ oic_db/queries/chargeback/select_chargeback.sql
@@ -1,27 +1,32 @@
 select 
-	chargeback.* 
-    ,clustered_receivable_customer.erp_customer_id
-    ,clustered_receivable_customer.full_name
-    ,clustered_receivable_customer.identification_financial_responsible
-from chargeback
+	cchbk.* 
+    ,chbk.* 
+    ,crc.erp_customer_id
+    ,crc.full_name
+    ,crc.identification_financial_responsible
+from chargeback chbk
 
-inner join receivable
-on receivable.conciliator_id = chargeback.conciliator_id
-and receivable.erp_clustered_receivable_id is not null -- Considerar somente os receivables que já foram convertidos em clustered_receivable, ou seja, que já foram aglutinados
-and receivable.erp_clustered_receivable_customer_id is not null -- Considerar somente os receivables que já foram convertidos em clustered_receivable, ou seja, que já foram aglutinados
+inner join clustered_chargeback cchbk
+on cchbk.id = chbk.erp_clustered_chargeback_id
 
-inner join clustered_receivable_customer
-on clustered_receivable_customer.erp_customer_id = receivable.erp_clustered_receivable_customer_id
+inner join receivable rec
+on rec.conciliator_id = chbk.conciliator_id
+and rec.erp_clustered_receivable_id is not null -- Considerar somente os receivables que já foram convertidos em clustered_receivable, ou seja, que já foram aglutinados
+and rec.erp_clustered_receivable_customer_id is not null -- Considerar somente os receivables que já foram convertidos em clustered_receivable, ou seja, que já foram aglutinados
+and rec.converted_smartfin <> 'yes'
 
-inner join order_to_cash
-on order_to_cash.country = chargeback.country
-and order_to_cash.id = receivable.order_to_cash_id
+inner join clustered_receivable_customer crc
+on crc.erp_customer_id = rec.erp_clustered_receivable_customer_id
 
-where chargeback.country = 'Brazil' -- Cada país deverá ter um processamento separado
-and order_to_cash.origin_system = 'smartsystem' -- Cada origem deverá ter um processamento separado
-and order_to_cash.operation = 'person_plan' -- Cada operação deverá ter um processamento separado
-and receivable.transaction_type = 'credit_card_recurring' -- Cada tipo de transação deverá ter um processamento separado
-and chargeback.erp_receipt_status_transaction = 'clustered_chargeback_created'
-and chargeback.erp_clustered_chargeback_id is not null
-and chargeback.erp_receipt_id is null
-and chargeback.concitiation_type = 'CHBK'; -- Considerar somente os retornos de comprovante de recebimento enviado pela conciliadora 
+inner join order_to_cash otc
+on otc.country = chbk.country
+and otc.id = rec.order_to_cash_id
+
+where chbk.country = 'Brazil' -- Cada país deverá ter um processamento separado
+and otc.origin_system = 'smartsystem' -- Cada origem deverá ter um processamento separado
+and otc.operation = 'person_plan' -- Cada operação deverá ter um processamento separado
+and rec.transaction_type = 'credit_card_recurring' -- Cada tipo de transação deverá ter um processamento separado
+and chbk.erp_receipt_status_transaction = 'clustered_chargeback_created'
+and chbk.erp_clustered_chargeback_id is not null
+and chbk.erp_receipt_id is null
+and chbk.concitiation_type = 'CHBK'; -- Considerar somente os retornos de comprovante de recebimento enviado pela conciliadora 
