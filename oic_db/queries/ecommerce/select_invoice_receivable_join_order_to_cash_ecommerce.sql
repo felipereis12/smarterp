d898aa19cf3c436ba1fd9a990aaa96602a259259
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-22 17:49:08 -0300

    Update select_invoice_receivable_join_order_to_cash_ecommerce.sql
    Script de seleção de vendas de uniformes do e-commerce:
     - Ajustadas algumas condições da query;

--- oic_db/queries/ecommerce/select_invoice_receivable_join_order_to_cash_ecommerce.sql
+++ oic_db/queries/ecommerce/select_invoice_receivable_join_order_to_cash_ecommerce.sql
@@ -4,6 +4,7 @@ select
     ,order_to_cash.erp_subsidiary
     ,order_to_cash.acronym
     ,receivable.* -- Utilizar aqui somente os campos necessários para a integração
+    ,invoice_customer.* -- Utilizar aqui somente os campos necessários para a integração
 	,invoice.* -- Utilizar aqui somente os campos necessários para a integração
 	,invoice_items.* -- Utilizar aqui somente os campos necessários para a integração    
     ,order_to_cash.* -- Utilizar aqui somente os campos necessários para a integração
@@ -15,14 +16,15 @@ on invoice_items.id_invoice = invoice.id
 inner join order_to_cash
 on order_to_cash.id = invoice.order_to_cash_id
 
+inner join invoice_customer
+on invoice_customer.order_to_cash_id = order_to_cash.id
+
 inner join receivable
-on order_to_cash.id = receivable.order_to_cash_id
+on receivable.order_to_cash_id = order_to_cash.id
 
 where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
 and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
 and order_to_cash.origin_system = 'magento' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'uniform_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
 and order_to_cash.to_generate_invoice = 'yes'
 and order_to_cash.erp_invoice_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and invoice.erp_invoice_customer_id is not null -- Filtrar somente as invoices cujos os clientes já foram integrados anteriormente
 and invoice.erp_invoice_id is null -- Filtrar somente as invoices que ainda não foram integrados com o erp
