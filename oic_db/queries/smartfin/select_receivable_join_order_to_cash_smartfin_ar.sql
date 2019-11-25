732784cc525ddace86fb72c3050067d1b8044f74
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-25 11:25:44 -0300

    Update select_receivable_join_order_to_cash_smartfin_ar.sql
    - Ajuste de campos da select para ser considerado na integração do Oracle;
    - Adição de campo converted_smartfin na tabela de de/para receivable_erp_configurations para diferenciar a transação de venda da unidade própria da transação de venda da unidade SmartFin;

--- oic_db/queries/smartfin/select_receivable_join_order_to_cash_smartfin_ar.sql
+++ oic_db/queries/smartfin/select_receivable_join_order_to_cash_smartfin_ar.sql
@@ -1,26 +1,51 @@
 select 
-     order_to_cash.erp_business_unit
-    ,order_to_cash.erp_legal_entity
-    ,order_to_cash.erp_subsidiary
-    ,order_to_cash.acronym
-	,receivable.* -- Utilizar aqui somente os campos necessários para a integração
-    ,order_to_cash.* -- Utilizar aqui somente os campos necessários para a integração
-    ,clustered_receivable_customer.* -- Utilizar aqui somente os campos necessários para a integração
-from receivable 
+   	 otc.erp_business_unit
+	,recg.erp_source_name
+	,recg.erp_type_transaction
+    ,recg.erp_payments_terms
+    ,recg.erp_currency_code
+    ,recg.erp_currency_conversion_type
+    ,rec.erp_clustered_receivable_id
+    ,crc.identification_financial_responsible
+    ,crc.full_name
+    ,rec.net_value
+    ,rec.price_list_value
+    ,rec.conciliator_id
+    ,rec.credit_card_brand
+    ,rec.contract_number
+    ,rec.transaction_type
+    ,rec.truncated_credit_card
+    ,rec.current_credit_card_installment
+    ,rec.total_credit_card_installment
+    ,rec.nsu
+    ,rec.authorization_code
+    ,rec.administration_tax_percentage
+    ,rec.administration_tax_value
+    ,rec.billing_date
+    ,rec.credit_date
+    ,rec.converted_smartfin
+from receivable rec
 
-inner join order_to_cash
-on order_to_cash.id = receivable.order_to_cash_id
+inner join order_to_cash otc
+on otc.id = rec.order_to_cash_id
 
-inner join clustered_receivable_customer
-on clustered_receivable_customer.identification_financial_responsible = order_to_cash.erp_receivable_customer_identification
+inner join clustered_receivable_customer crc
+on crc.identification_financial_responsible = otc.erp_receivable_customer_identification
 
-where order_to_cash.country = 'Brazil' -- Integração em paralalo por operação do país
-and order_to_cash.erp_subsidiary = 'BR020001' -- Neste caso como haverá uma integração separada para os movimentos Smartfin esse filtro deverá ser fixo para tais movimentos
-and order_to_cash.origin_system = 'smartsystem' -- Integração em paralalo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'person_plan' -- Integração em paralalo por operação (plano de alunos, plano corporativo, etc...)
-and order_to_cash.erp_receipt_status_transaction = 'clustered_receivable_created' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and order_to_cash.to_generate_receivable = 'yes'
-and receivable.erp_clustered_receivable_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable
-and receivable.erp_clustered_receivable_customer_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable_customer 
-and receivable.erp_receivable_id is null -- Filtrar somente os receivables que ainda não foram integrados com o erp
-and receivable.transaction_type = 'credit_card_recurring' -- Integração em paralalo por tipo de transação (Cartão de crédito recorrente, cartão de débito recorrente, débito em conta, boleto etc...)
+left join receivable_erp_configurations recg
+on recg.country = otc.country
+and recg.origin_system = otc.origin_system
+and recg.operation = otc.operation
+and recg.transaction_type = rec.transaction_type
+and recg.converted_smartfin = rec.converted_smartfin
+
+where otc.country = 'Brazil' -- Integração em paralalo por operação do país
+and otc.erp_subsidiary = 'BR020001' -- Neste caso como haverá uma integração separada para os movimentos Smartfin esse filtro deverá ser fixo para tais movimentos
+and otc.origin_system = 'smartsystem' -- Integração em paralalo por origem (SmartFit, BioRitmo, etc...)
+and otc.operation = 'person_plan' -- Integração em paralalo por operação (plano de alunos, plano corporativo, etc...)
+and otc.erp_receivable_status_transaction = 'clustered_receivable_created' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
+and otc.to_generate_receivable = 'yes'
+and rec.erp_clustered_receivable_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable
+and rec.erp_clustered_receivable_customer_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable_customer 
+and rec.erp_receivable_id is null -- Filtrar somente os receivables que ainda não foram integrados com o erp
+and rec.transaction_type = 'credit_card_recurring' -- Integração em paralalo por tipo de transação (Cartão de crédito recorrente, cartão de débito recorrente, débito em conta, boleto etc...)
