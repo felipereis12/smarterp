6a650cb099e8729e0752a18edff9f816b5a81984
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-25 11:34:53 -0300

    Update select_all_tables.sql
    none

--- oic_db/queries/various/select_all_tables.sql
+++ oic_db/queries/various/select_all_tables.sql
@@ -3,6 +3,7 @@ select * from order_to_cash;
 -- select * from clustered_receivable_customer;
 select * from receivable;
 select * from clustered_receivable;
+select * from receivable_erp_configurations;
 select * from invoice;
 select * from invoice_items;
 -- select * from chargeback;
