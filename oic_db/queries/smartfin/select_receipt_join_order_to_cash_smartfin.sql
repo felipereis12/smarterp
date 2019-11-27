514ff6eace870cfaa793f5cf4ff20b12e9c85de2
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-27 15:46:40 -0300

    Update select_receipt_join_order_to_cash_smartfin.sql
    - Para os casos de Receipt (baixa) não havera detalhamento de items e cabeçalho no Oracle, portanto apenas uma linha com o valor liquido deve ser considerado;

--- oic_db/queries/smartfin/select_receipt_join_order_to_cash_smartfin.sql
+++ oic_db/queries/smartfin/select_receipt_join_order_to_cash_smartfin.sql
@@ -1,5 +1,12 @@
-select 
-	 otc.erp_business_unit
+/*
+gross_value => valor serviço/mercadororia + juros/mora
+net_value => valor serviço/mercadororia + juros/mora - taxa operadora
+interest_value => juros/mora
+*/
+
+select 	
+	otc.id
+	,otc.erp_business_unit
 	,recg.erp_source_name
 	,recg.erp_type_transaction
     ,recg.erp_payments_terms
@@ -39,16 +46,21 @@ on recg.country = otc.country
 and recg.origin_system = otc.origin_system
 and recg.operation = otc.operation
 and recg.transaction_type = rec.transaction_type
+and recg.memoline_setting = 'gross_value'
 
 left join receipt_from_to_version rftv
-on rftv.erp_business_unit = otc.erp_business_unit
+on rftv.origin_system = otc.origin_system
+and rftv.order_to_cash_operation = otc.operation
+and rftv.erp_business_unit = otc.erp_business_unit
 and rftv.receivable_transaction_type = rec.transaction_type
 and rftv.erp_receivable_customer_identification = crc.identification_financial_responsible
 and rftv.created_at = 	(
 							select
 								max(rftv_v2.created_at) as created_at
 							from receipt_from_to_version rftv_v2
-							where rftv_v2.erp_business_unit = rftv.erp_business_unit
+							where rftv_v2.origin_system = rftv.origin_system
+							and rftv_v2.order_to_cash_operation = rftv.order_to_cash_operation
+                            and rftv_v2.erp_business_unit = rftv.erp_business_unit
 							and rftv_v2.receivable_transaction_type = rftv.receivable_transaction_type
 							and rftv_v2.erp_receivable_customer_identification = rftv.erp_receivable_customer_identification
 						)
@@ -61,4 +73,6 @@ and otc.erp_receivable_status_transaction = 'clustered_receivable_created' -- Fi
 and rec.erp_clustered_receivable_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable
 and rec.erp_clustered_receivable_customer_id is not null -- Filtrar somente os receivables que possui relacionamento com a clustered_receivable_customer 
 and rec.erp_receivable_id is not null -- Filtrar somente os receivables que já foram integrados no erp e devem ser baixados
+and rec.erp_receivable_id is not null -- Filtrar somente os receivables que já foram integrados no erp e devem ser baixados
+and rec.net_value > 0
 and rec.transaction_type in ('debit_account_recurring','cash','boleto') -- Neste caso a integração deverá filtrar somente os receivables cujos métodos de recebimentos são débito em conta corrente, dinheiro ou boleto 
