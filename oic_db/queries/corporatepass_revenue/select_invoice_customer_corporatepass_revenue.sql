24475238ca503d98f195b854b4373f5e146c23c4
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-27 18:54:52 -0300

    Update select_invoice_customer_corporatepass_revenue.sql
     - Ajuste em condição de query para considerar somente os movimentos de corporatepass de venda;

--- oic_db/queries/corporatepass_revenue/select_invoice_customer_corporatepass_revenue.sql
+++ oic_db/queries/corporatepass_revenue/select_invoice_customer_corporatepass_revenue.sql
@@ -26,7 +26,7 @@ and invoice_customer.identification_financial_responsible = invoice_customer_com
 where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
 and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
 and order_to_cash.origin_system = 'corporatepass' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'corporate_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
+and order_to_cash.operation = 'renevue' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
 and order_to_cash.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
 and order_to_cash.to_generate_customer = 'yes'
 and (
@@ -76,7 +76,7 @@ on order_to_cash.id = invoice_customer.order_to_cash_id
 where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
 and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
 and order_to_cash.origin_system = 'corporatepass' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.operation = 'corporate_plan' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
+and order_to_cash.operation = 'renevue' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
 and order_to_cash.erp_invoice_customer_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
 and order_to_cash.to_generate_customer = 'yes'
 and invoice_customer.erp_customer_id is null;
