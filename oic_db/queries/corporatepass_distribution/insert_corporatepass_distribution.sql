9496943f3e523fd889cf9b9de53e1dd8ffdc1af6
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-28 15:48:59 -0300

    Update insert_corporatepass_distribution.sql
     Script para inserir registros de teste de corporatepass repasse

--- oic_db/queries/corporatepass_distribution/insert_corporatepass_distribution.sql
+++ oic_db/queries/corporatepass_distribution/insert_corporatepass_distribution.sql
@@ -35,7 +35,7 @@ insert into oic_db.order_to_cash
 				values(
 				null, -- order_to_cash_id_smartfin
 				'Brazil', -- country
-				1, -- unity_identification
+				15, -- unity_identification
 				null, -- erp_business_unit
 				null, -- erp_legal_entity
 				null, -- erp_subsidiary
@@ -91,6 +91,88 @@ order_to_cash.erp_subsidiary = organization_from_to_version.erp_subsidiary,
 order_to_cash.acronym = organization_from_to_version.acronym
 
 where order_to_cash.id = @order_to_cash_id;
+
+insert into oic_db.receivable
+				(order_to_cash_id,
+				erp_receivable_id,
+				erp_receipt_id,
+				erp_receivable_customer_id,
+				erp_clustered_receivable_id,
+				erp_clustered_receivable_customer_id,
+				is_smartfin,
+				converted_smartfin,
+				type_smartfin,
+				receivable_id_smartfin,
+				ecommerce_payment_terms,
+				erp_payment_terms,
+				conciliator_id,
+				transaction_type,
+				contract_number,
+				credit_card_brand,
+				truncated_credit_card,
+				current_credit_card_installment,
+				total_credit_card_installment,
+				nsu,
+				authorization_code,
+				price_list_value,
+				gross_value,
+				net_value,
+				interest_value,
+				administration_tax_percentage,
+				administration_tax_value,
+				antecipation_tax_percentage,
+				antecipation_tax_value,
+				billing_date,
+				credit_date,
+				conciliator_filename,
+				acquirer_bank_filename,
+				registration_gym_student,
+				fullname_gym_student,
+				identification_gym_student,
+				erp_filename,
+				erp_line_in_file)
+				values
+				(@order_to_cash_id, -- order_to_cash_id
+				null, -- erp_receivable_id
+				null, -- erp_receipt_id
+				null, -- erp_receivable_customer_id
+				null, -- erp_clustered_receivable_id
+				null, -- erp_clustered_receivable_customer_id
+				null, -- is_smartfin
+				'no', -- converted_smartfin
+				null, -- type_smartfin
+				null, -- receivable_id_smartfin
+				null, -- ecommerce_payment_terms
+				null, -- erp_payment_terms
+				null, -- conciliator_id
+				'boleto', -- transaction_type
+				null, -- contract_number
+				null, -- credit_card_brand
+				null, -- truncated_credit_card
+				null, -- current_credit_card_installment
+				null, -- total_credit_card_installment
+				null, -- nsu
+				null, -- authorization_code
+				100.15, -- price_list_value
+				100.15, -- gross_value
+				100.15, -- net_value
+				0, -- interest_value
+				0, -- administration_tax_percentage
+				0, -- administration_tax_value
+				0, -- antecipation_tax_percentage
+				0, -- antecipation_tax_value
+				'2019-11-28', -- billing_date
+				'2019-11-28',-- credit_date
+				null, -- conciliator_filename
+				null, -- acquirer_bank_filename
+				null, -- registration_gym_student
+				null, -- fullname_gym_student
+				null, -- identification_gym_student
+				null, -- erp_filename
+				null); -- erp_line_in_file
+
+-- saves the auto increment id from receivable table
+set @receivable_id = last_insert_id();
 
 insert into oic_db.invoice
 				(order_to_cash_id,
@@ -183,3 +265,84 @@ set invoice_items.erp_item_ar_id = product_from_to_version.erp_item_ar_id,
 invoice_items.erp_gl_segment_product = product_from_to_version.erp_gl_segment_id
 
 where invoice_items.id = @invoice_id_item ;
+
+
+insert into oic_db.payable
+			(unity_identification,
+			erp_business_unit,
+			erp_legal_entity,
+			erp_subsidiary,
+			acronym,
+			erp_payable_id,
+			receivable_id,
+			erp_receivable_id,
+			erp_clustered_receivable_id,
+			erp_payable_receipt_id,
+			erp_supplier_id,
+			supplier_identification,
+			issue_date,
+			due_date,
+			erp_payable_send_to_erp_at,
+			erp_payable_returned_from_erp_at,
+			erp_payable_status_transaction,
+			erp_payable_log,
+			erp_receipt_send_to_erp_at,
+			erp_receipt_returned_from_erp_at,
+			erp_receipt_status_transaction,
+			erp_receipt_log,
+			erp_filename,
+			erp_line_in_file)
+			values
+			(1, -- unity_identification
+			null, -- erp_business_unit
+			null, -- erp_legal_entity
+			null, -- erp_subsidiary
+			null, -- acronym
+			null, -- erp_payable_id
+			@receivable_id, -- receivable_id
+			null, -- erp_receivable_id
+			null, -- erp_clustered_receivable_id
+			null, -- erp_payable_receipt_id
+			null, -- erp_supplier_id
+			'07594978001654', -- supplier_identification
+			'2019-11-28', -- issue_date
+			'2019-11-30', -- due_date
+			null, -- erp_payable_send_to_erp_at
+			null, -- erp_payable_returned_from_erp_at
+			'waiting_to_be_process', -- erp_payable_status_transaction
+			null, -- erp_payable_log
+			null, -- erp_receipt_send_to_erp_at
+			null, -- erp_receipt_returned_from_erp_at
+			'doesnt_need_to_be_process', -- erp_receipt_status_transaction
+			null, -- erp_receipt_log
+			null, -- erp_filename
+			null); -- erp_line_in_file
+
+-- saves the auto increment id from order_to_cash table
+set @payable_id = last_insert_id();
+
+-- updates the organizations fields from order_to_cash table. Note that the last version from organization_from_to_version must be always considered
+update payable
+
+inner join organization_from_to 
+on payable.unity_identification = organization_from_to.unity_identification
+
+inner join organization_from_to_version
+on organization_from_to_version.organization_from_to_unity_identification = organization_from_to.unity_identification
+and organization_from_to_version.created_at = ( 
+												select 
+													max(organization_from_to_version_v2.created_at) as max_created_at
+												from organization_from_to_version organization_from_to_version_v2
+                                                where organization_from_to_version_v2.organization_from_to_unity_identification = organization_from_to_version.organization_from_to_unity_identification
+												)
+
+inner join supplier
+on supplier.identification_financial_responsible = payable.supplier_identification
+
+set payable.erp_business_unit = organization_from_to_version.erp_business_unit,
+payable.erp_legal_entity = organization_from_to_version.erp_legal_entity,
+payable.erp_subsidiary = organization_from_to_version.erp_subsidiary,
+payable.acronym = organization_from_to_version.acronym,
+payable.erp_supplier_id = supplier.erp_supplier_id
+
+where payable.id = @payable_id;
