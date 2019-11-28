5c57b97e524577ec450c7f3aba59d753e813b6be
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-11-28 15:49:31 -0300

    Create select_invoice_corporatepass_distribution.sql
    - Script para seleção de invoice de corporatepass repasse para integrar no Oracle

---
+++ oic_db/queries/corporatepass_distribution/select_invoice_corporatepass_distribution.sql
@@ -0,0 +1,61 @@
+select 
+     otc.erp_business_unit
+    ,iec.erp_source_name
+    ,iec.erp_type_transaction
+    ,iec.erp_payments_terms
+    ,iec.erp_currency_code
+    ,iec.erp_currency_conversion_type
+    ,iec.erp_interface_line_context
+    ,iec.erp_product_category_fiscal
+    ,otc.front_id -- id do front
+    ,otc.fin_id -- id do fin
+    ,otc.conciliator_id -- id do conciliator_id
+    ,otc.minifactu_id -- id do minifactu
+    ,crc.identification_financial_responsible -- cpf/cnpj do responsável financeiro
+    ,crc.full_name -- nome do responsável financeiro
+    ,iit.id -- id do item da invoice
+    ,iit.erp_item_ar_id -- código do item do ar no Oracle 
+    ,iit.erp_gl_segment_product -- código do segmento contábil de produto
+    ,iit.quantity -- Quantidade do item de venda
+    ,iit.sale_price -- Preço praticado
+    ,iit.list_price -- Preço de lista
+from invoice inv
+
+inner join invoice_items iit
+on iit.id_invoice = inv.id
+
+inner join order_to_cash otc
+on otc.id = inv.order_to_cash_id
+
+inner join clustered_receivable_customer crc
+on crc.identification_financial_responsible = otc.erp_receivable_customer_identification
+
+left join invoice_erp_configurations iec
+on iec.country = otc.country
+and iec.erp_business_unit = otc.erp_business_unit
+and iec.erp_legal_entity = otc.erp_legal_entity
+and iec.erp_subsidiary = otc.erp_subsidiary
+and iec.origin_system = otc.origin_system
+and iec.operation = otc.operation
+
+inner join organization_from_to_version oftv
+on oftv.erp_business_unit = otc.erp_business_unit
+and oftv.erp_legal_entity = otc.erp_legal_entity
+and oftv.erp_subsidiary = otc.erp_subsidiary
+and oftv.created_at = 	(
+							select
+								max(oftv_v2.created_at) as created_at
+							from organization_from_to_version oftv_v2
+                            where oftv_v2.erp_business_unit = oftv.erp_business_unit
+                            and oftv_v2.erp_legal_entity = oftv.erp_legal_entity
+                            and oftv_v2.erp_subsidiary = oftv.erp_subsidiary
+						)
+
+where otc.country = 'Brazil' -- Integração em paralelo por operação do país
+and otc.erp_subsidiary = 'BR010015' -- Filtro por filial (loop automático)
+and otc.origin_system = 'corporatepass' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
+and otc.operation = 'distribution' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
+and otc.to_generate_invoice = 'yes'
+and otc.erp_invoice_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
+and inv.erp_invoice_id is null -- Filtrar somente as invoices que ainda não foram integrados com o erp
+and day(current_date()) <= oftv.cutoff_limit_day -- Regra de cutoff
