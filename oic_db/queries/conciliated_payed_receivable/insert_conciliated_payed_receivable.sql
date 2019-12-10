f019147d834c4bd3c6cbdaaaec448d4df376c121
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-12-10 16:28:41 -0300

    Update insert_conciliated_payed_receivable.sql
    none

--- oic_db/queries/conciliated_payed_receivable/insert_conciliated_payed_receivable.sql
+++ oic_db/queries/conciliated_payed_receivable/insert_conciliated_payed_receivable.sql
@@ -1,38 +1,38 @@
-INSERT INTO `oic_db`.`conciliated_payed_receivable`
-(`country`, -- Criar uma integração para cada país. No caso do Brasil será um job para a conciliadora Concil
-`erp_receipt_id`, -- Id único e imutável do receipt no Oracle
-`erp_receipt_status_transaction`, -- Ao ler o arquivo da Concil preencher com 'waiting_to_be_process'
-`erp_receipt_sent_to_erp_at`, -- TimeStamp de quando o registro foi enviado para o Oracle
-`erp_receipt_returned_from_erp_at`, -- TimeStamp de quando o registro retornou do Oracle
-`erp_receipt_log`, -- Este campo deverá ser preenchido no retorno do Oracle
-`erp_receivable_customer_id`, -- Desconsiderar
-`conciliator_id`, -- Utilizar a coluna COD_ERP do arquivo de retorno da Concil
-`concitiation_type`, -- Utilizar a coluna TIPO_LANCAMENTO do arquivo de retorno da Concil - ao ler o arquivo para ser gravado nessa tabela considerar somente o tipo PCV
-`conciliation_description`, -- Utilizar a coluna DESCRICAO do arquivo de retorno da Concil 
-`transaction_type`, -- Utilizar a coluna TIPO_TRANSACAO do arquivo de retorno da Concil - converter CREDITO para credit_card e DEBITO para debit_card
-`contract_number`, -- Utilizar a coluna ESTABELECIMENTO do arquivo de retorno da Concil 
-`credit_card_brand`, -- Utilizar a coluna BANDEIRA do arquivo de retorno da Concil - converter os valores AMEX para americanexpress, MASTER para MASTER, HIPERCARD para hipercard, ELO para elo, VISA para VISA e DINERS para diners
-`truncated_credit_card`, -- Utilizar a coluna MASC_CARTAO do arquivo de retorno da Concil
-`current_credit_card_installment`, -- Utilizar a coluna NRO_PARCELA do arquivo de retorno da Concil -- Left(NRO_PARCELA,2)
-`total_credit_card_installment`, -- Utilizar a coluna NRO_PARCELA do arquivo de retorno da Concil -- Right(NRO_PARCELA,2)
-`nsu`, -- Utilizar a coluna NSU do arquivo de retorno da Concil 
-`authorization_code`, -- Utilizar a coluna AUTORIZACAO do arquivo de retorno da Concil 
-`payment_lot`, -- Utilizar a coluna NR_RO do arquivo de retorno da Concil 
-`price_list_value`, -- Desconsiderar
-`gross_value`, -- Utilizar a coluna VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`net_value`, -- Utilizar a coluna VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`interest_value`, -- Desconsiderar
-`administration_tax_percentage`, -- Utilizar a coluna TAXA/VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`administration_tax_value`, -- Utilizar a coluna TAXA do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`antecipation_tax_percentage`, -- Utilizar a coluna VLR_DESC_ANTECIP/VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`antecipation_tax_value`,-- Utilizar a coluna VLR_DESC_ANTECIP do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
-`billing_date`, -- Utilizar a coluna DT_VENDA do arquivo de retorno da Concil 
-`credit_date`, -- Utilizar a coluna DT_PAGAMENTO do arquivo de retorno da Concil 
-`bank_number`, -- Utilizar a coluna NRO_BANCO do arquivo de retorno da Concil 
-`bank_branch`, -- Utilizar a coluna NRO_AGENCIA do arquivo de retorno da Concil 
-`bank_account`, -- Utilizar a coluna NRO_CONTA do arquivo de retorno da Concil 
-`conciliator_filename`, -- Aqui deve ser gravado o nome do arquivo lido
-`acquirer_bank_filename`) -- Utilizar a coluna NOME_ARQUIVO do arquivo de retorno da Concil 
+INSERT INTO conciliated_payed_receivable
+(country, -- Criar uma integração para cada país. No caso do Brasil será um job para a conciliadora Concil
+erp_receipt_id, -- Id único e imutável do receipt no Oracle
+erp_receipt_status_transaction, -- Ao ler o arquivo da Concil preencher com 'waiting_to_be_process'
+erp_receipt_sent_to_erp_at, -- TimeStamp de quando o registro foi enviado para o Oracle
+erp_receipt_returned_from_erp_at, -- TimeStamp de quando o registro retornou do Oracle
+erp_receipt_log, -- Este campo deverá ser preenchido no retorno do Oracle
+erp_receivable_customer_id, -- Desconsiderar
+conciliator_id, -- Utilizar a coluna COD_ERP do arquivo de retorno da Concil
+concitiation_type, -- Utilizar a coluna TIPO_LANCAMENTO do arquivo de retorno da Concil - ao ler o arquivo para ser gravado nessa tabela considerar somente o tipo PCV
+conciliation_description, -- Utilizar a coluna DESCRICAO do arquivo de retorno da Concil 
+transaction_type, -- Utilizar a coluna TIPO_TRANSACAO do arquivo de retorno da Concil - converter CREDITO para credit_card e DEBITO para debit_card
+contract_number, -- Utilizar a coluna ESTABELECIMENTO do arquivo de retorno da Concil 
+credit_card_brand, -- Utilizar a coluna BANDEIRA do arquivo de retorno da Concil - converter os valores AMEX para americanexpress, MASTER para MASTER, HIPERCARD para hipercard, ELO para elo, VISA para VISA e DINERS para diners
+truncated_credit_card, -- Utilizar a coluna MASC_CARTAO do arquivo de retorno da Concil
+current_credit_card_installment, -- Utilizar a coluna NRO_PARCELA do arquivo de retorno da Concil -- Left(NRO_PARCELA,2)
+total_credit_card_installment, -- Utilizar a coluna NRO_PARCELA do arquivo de retorno da Concil -- Right(NRO_PARCELA,2)
+nsu, -- Utilizar a coluna NSU do arquivo de retorno da Concil 
+authorization_code, -- Utilizar a coluna AUTORIZACAO do arquivo de retorno da Concil 
+payment_lot, -- Utilizar a coluna NR_RO do arquivo de retorno da Concil 
+price_list_value, -- Desconsiderar
+gross_value, -- Utilizar a coluna VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+net_value, -- Utilizar a coluna VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+interest_value, -- Desconsiderar
+administration_tax_percentage, -- Utilizar a coluna TAXA/VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+administration_tax_value, -- Utilizar a coluna TAXA do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+antecipation_tax_percentage, -- Utilizar a coluna VLR_DESC_ANTECIP/VLR_PAGO do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+antecipation_tax_value,-- Utilizar a coluna VLR_DESC_ANTECIP do arquivo de retorno da Concil (dividir por 100, pois o número do arquivo está sem pontuação decimal)
+billing_date, -- Utilizar a coluna DT_VENDA do arquivo de retorno da Concil 
+credit_date, -- Utilizar a coluna DT_PAGAMENTO do arquivo de retorno da Concil 
+bank_number, -- Utilizar a coluna NRO_BANCO do arquivo de retorno da Concil 
+bank_branch, -- Utilizar a coluna NRO_AGENCIA do arquivo de retorno da Concil 
+bank_account, -- Utilizar a coluna NRO_CONTA do arquivo de retorno da Concil 
+conciliator_filename, -- Aqui deve ser gravado o nome do arquivo lido
+acquirer_bank_filename) -- Utilizar a coluna NOME_ARQUIVO do arquivo de retorno da Concil 
 VALUES
 ('Brazil', -- country
 null, -- erp_receipt_id
@@ -69,41 +69,41 @@ null, -- authorization_code
 null, -- conciliator_filename
 null); -- acquirer_bank_filename
 
-INSERT INTO `oic_db`.`conciliated_payed_receivable`
-(`country`,
-`erp_receipt_id`,
-`erp_receipt_status_transaction`,
-`erp_receipt_sent_to_erp_at`,
-`erp_receipt_returned_from_erp_at`,
-`erp_receipt_log`,
-`erp_receivable_customer_id`,
-`conciliator_id`,
-`concitiation_type`,
-`conciliation_description`,
-`transaction_type`,
-`contract_number`,
-`credit_card_brand`,
-`truncated_credit_card`,
-`current_credit_card_installment`,
-`total_credit_card_installment`,
-`nsu`,
-`authorization_code`,
-`payment_lot`,
-`price_list_value`,
-`gross_value`,
-`net_value`,
-`interest_value`,
-`administration_tax_percentage`,
-`administration_tax_value`,
-`antecipation_tax_percentage`, 
-`antecipation_tax_value`,
-`billing_date`,
-`credit_date`,
-`bank_number`,
-`bank_branch`,
-`bank_account`,
-`conciliator_filename`,
-`acquirer_bank_filename`)
+INSERT INTO conciliated_payed_receivable
+(country,
+erp_receipt_id,
+erp_receipt_status_transaction,
+erp_receipt_sent_to_erp_at,
+erp_receipt_returned_from_erp_at,
+erp_receipt_log,
+erp_receivable_customer_id,
+conciliator_id,
+concitiation_type,
+conciliation_description,
+transaction_type,
+contract_number,
+credit_card_brand,
+truncated_credit_card,
+current_credit_card_installment,
+total_credit_card_installment,
+nsu,
+authorization_code,
+payment_lot,
+price_list_value,
+gross_value,
+net_value,
+interest_value,
+administration_tax_percentage,
+administration_tax_value,
+antecipation_tax_percentage, 
+antecipation_tax_value,
+billing_date,
+credit_date,
+bank_number,
+bank_branch,
+bank_account,
+conciliator_filename,
+acquirer_bank_filename)
 VALUES
 ('Brazil', -- country
 null, -- erp_receipt_id
@@ -141,41 +141,41 @@ null, -- conciliator_filename
 null); -- acquirer_bank_filename
 
 
-INSERT INTO `oic_db`.`conciliated_payed_receivable`
-(`country`,
-`erp_receipt_id`,
-`erp_receipt_status_transaction`,
-`erp_receipt_sent_to_erp_at`,
-`erp_receipt_returned_from_erp_at`,
-`erp_receipt_log`,
-`erp_receivable_customer_id`,
-`conciliator_id`,
-`concitiation_type`,
-`conciliation_description`,
-`transaction_type`,
-`contract_number`,
-`credit_card_brand`,
-`truncated_credit_card`,
-`current_credit_card_installment`,
-`total_credit_card_installment`,
-`nsu`,
-`authorization_code`,
-`payment_lot`,
-`price_list_value`,
-`gross_value`,
-`net_value`,
-`interest_value`,
-`administration_tax_percentage`,
-`administration_tax_value`,
-`antecipation_tax_percentage`, 
-`antecipation_tax_value`,
-`billing_date`,
-`credit_date`,
-`bank_number`,
-`bank_branch`,
-`bank_account`,
-`conciliator_filename`,
-`acquirer_bank_filename`)
+INSERT INTO conciliated_payed_receivable
+(country,
+erp_receipt_id,
+erp_receipt_status_transaction,
+erp_receipt_sent_to_erp_at,
+erp_receipt_returned_from_erp_at,
+erp_receipt_log,
+erp_receivable_customer_id,
+conciliator_id,
+concitiation_type,
+conciliation_description,
+transaction_type,
+contract_number,
+credit_card_brand,
+truncated_credit_card,
+current_credit_card_installment,
+total_credit_card_installment,
+nsu,
+authorization_code,
+payment_lot,
+price_list_value,
+gross_value,
+net_value,
+interest_value,
+administration_tax_percentage,
+administration_tax_value,
+antecipation_tax_percentage, 
+antecipation_tax_value,
+billing_date,
+credit_date,
+bank_number,
+bank_branch,
+bank_account,
+conciliator_filename,
+acquirer_bank_filename)
 VALUES
 ('Brazil', -- country
 null, -- erp_receipt_id
