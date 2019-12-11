510440515960b991bbd354a492da2486d735fa19
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-12-11 15:38:22 -0300

    Update select_invoice_receivable_join_order_to_cash_ecommerce.sql
     - Alteração para contemplar campos de de/para Oracle

--- oic_db/queries/ecommerce/select_invoice_receivable_join_order_to_cash_ecommerce.sql
+++ oic_db/queries/ecommerce/select_invoice_receivable_join_order_to_cash_ecommerce.sql
@@ -1,30 +1,63 @@
 select 
-     order_to_cash.erp_business_unit
-    ,order_to_cash.erp_legal_entity
-    ,order_to_cash.erp_subsidiary
-    ,order_to_cash.acronym
-    ,receivable.* -- Utilizar aqui somente os campos necessários para a integração
-    ,invoice_customer.* -- Utilizar aqui somente os campos necessários para a integração
-	,invoice.* -- Utilizar aqui somente os campos necessários para a integração
-	,invoice_items.* -- Utilizar aqui somente os campos necessários para a integração    
-    ,order_to_cash.* -- Utilizar aqui somente os campos necessários para a integração
-from invoice 
+     otc.erp_business_unit
+    ,iec.erp_source_name
+    ,iec.erp_type_transaction
+    ,iec.erp_payments_terms
+    ,iec.erp_currency_code
+    ,iec.erp_currency_conversion_type
+    ,iec.erp_interface_line_context
+    ,iec.erp_payment_code
+    ,iec.erp_set_of_books_id    
+    ,iec.erp_product_category_fiscal
+    ,iec.erp_attribute_category
+    ,iec.warehouse_id
+    ,iec.erp_product_category_fiscal
+    ,rec.erp_clustered_receivable_id -- id do aglutinado
+    ,otc.front_id -- id do front
+    ,otc.fin_id -- id do fin
+    ,otc.conciliator_id -- id do conciliator_id
+    ,otc.minifactu_id -- id do minifactu
+    ,ivcr.identification_financial_responsible -- cpf/cnpj do responsável financeiro
+    ,ivcr.full_name -- nome do responsável financeiro
+    ,rec.nsu -- nsu
+    ,rec.authorization_code -- código de autorização
+    ,rec.credit_card_brand
+	,rec.contract_number
+	,rec.erp_clustered_receivable_id
+	,otc.erp_subsidiary
+    ,iit.id -- id do item da invoice
+    ,iit.erp_item_ar_id -- código do item do ar no Oracle 
+    ,iit.erp_gl_segment_product -- código do segmento contábil de produto
+    ,iit.quantity -- Quantidade do item de venda
+    ,iit.sale_price -- Preço praticado
+    ,iit.list_price -- Preço de lista
+    ,if(month(rec.billing_date)=month(current_date()),rec.billing_date,current_date()) as erp_trx_date
+    ,if(month(rec.billing_date)=month(current_date()),rec.billing_date,current_date()) as erp_gl_date   
+from invoice inv
 
-inner join invoice_items
-on invoice_items.id_invoice = invoice.id
+inner join invoice_items iit
+on iit.id_invoice = inv.id
 
-inner join order_to_cash
-on order_to_cash.id = invoice.order_to_cash_id
+inner join order_to_cash otc
+on otc.id = inv.order_to_cash_id
 
-inner join invoice_customer
-on invoice_customer.order_to_cash_id = order_to_cash.id
+inner join invoice_customer ivcr
+on ivcr.order_to_cash_id = otc.id
 
-inner join receivable
-on receivable.order_to_cash_id = order_to_cash.id
+inner join receivable rec
+on rec.order_to_cash_id = otc.id
 
-where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
-and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
-and order_to_cash.origin_system = 'magento' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
-and order_to_cash.to_generate_invoice = 'yes'
-and order_to_cash.erp_invoice_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and invoice.erp_invoice_id is null -- Filtrar somente as invoices que ainda não foram integrados com o erp
+inner join invoice_erp_configurations iec
+on iec.country = otc.country
+and iec.erp_business_unit = otc.erp_business_unit
+and iec.erp_legal_entity = otc.erp_legal_entity
+and iec.erp_subsidiary = otc.erp_subsidiary
+and iec.origin_system = otc.origin_system
+and iec.operation = otc.operation
+
+where otc.country = 'Brazil' -- Integração em paralelo por operação do país
+and otc.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
+and otc.origin_system = 'magento' -- Integração em paralelo por origem (SmartFit, BioRitmo, etc...)
+and otc.to_generate_invoice = 'yes'
+and otc.erp_invoice_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
+and inv.erp_invoice_id is null -- Filtrar somente as invoices que ainda não foram integrados com o erp
