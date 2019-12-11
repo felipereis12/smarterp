036154ecd7bf99a2daff66ca1aabde46bf04492c
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-12-11 16:45:04 -0300

    Update select_invoice_receivable_join_order_to_cash_corporatepass_revenue.sql
     - Ajuste: adição de relacinamento com invoice_erp_configurations

--- oic_db/queries/corporatepass_revenue/select_invoice_receivable_join_order_to_cash_corporatepass_revenue.sql
+++ oic_db/queries/corporatepass_revenue/select_invoice_receivable_join_order_to_cash_corporatepass_revenue.sql
@@ -1,22 +1,55 @@
 select  
-     order_to_cash.erp_business_unit
-    ,order_to_cash.erp_legal_entity
-    ,order_to_cash.erp_subsidiary
-    ,order_to_cash.acronym
-    ,receivable.* -- Utilizar aqui somente os campos necessários para a integração
-	,invoice.* -- Utilizar aqui somente os campos necessários para a integração
-	,invoice_items.* -- Utilizar aqui somente os campos necessários para a integração    
-    ,order_to_cash.* -- Utilizar aqui somente os campos necessários para a integração
-from invoice 
+     otc.erp_business_unit
+    ,iec.erp_source_name
+    ,iec.erp_type_transaction
+    ,iec.erp_payments_terms
+    ,iec.erp_currency_code
+    ,iec.erp_currency_conversion_type
+    ,iec.erp_interface_line_context
+    ,iec.erp_payment_code
+    ,iec.erp_set_of_books_id    
+    ,iec.erp_product_category_fiscal
+    ,iec.erp_attribute_category
+    ,iec.warehouse_id
+    ,otc.id as id_otc-- id da order_to_cash
+    ,ifnull(rec.erp_clustered_receivable_id,rec_v2.erp_clustered_receivable_id) as erp_clustered_receivable_id -- id do aglutinado
+    ,otc.front_id -- id do front
+    ,otc.fin_id -- id do fin
+    ,otc.conciliator_id -- id do conciliator_id
+    ,otc.minifactu_id -- id do minifactu
+    ,ivcr.identification_financial_responsible -- cpf/cnpj do responsável financeiro
+    ,ivcr.full_name -- nome do responsável financeiro
+    ,rec.nsu -- nsu
+    ,rec.authorization_code -- código de autorização
+    ,rec.credit_card_brand
+	,rec.contract_number
+	,otc.erp_subsidiary
+    ,iit.id  as id_otc_item -- id do item da invoice
+    ,iit.erp_item_ar_id -- código do item do ar no Oracle 
+    ,iit.erp_gl_segment_product -- código do segmento contábil de produto
+    ,iit.quantity -- Quantidade do item de venda
+    ,iit.sale_price -- Preço praticado
+    ,iit.list_price -- Preço de lista
+    ,if(month(rec.billing_date)=month(current_date()),rec.billing_date,current_date()) as erp_trx_date
+    ,if(month(rec.billing_date)=month(current_date()),rec.billing_date,current_date()) as erp_gl_date    
+from invoice inv
 
-inner join invoice_items
-on invoice_items.id_invoice = invoice.id
+inner join invoice_items iit
+on iit.id_invoice = inv.id
 
-inner join order_to_cash
-on order_to_cash.id = invoice.order_to_cash_id
+inner join order_to_cash otc
+on otc.id = inv.order_to_cash_id
 
-inner join receivable
-on order_to_cash.id = receivable.order_to_cash_id
+inner join receivable rec
+on otc.id = rec.order_to_cash_id
+
+inner join invoice_erp_configurations iec
+on iec.country = otc.country
+and iec.erp_business_unit = otc.erp_business_unit
+and iec.erp_legal_entity = otc.erp_legal_entity
+and iec.erp_subsidiary = otc.erp_subsidiary
+and iec.origin_system = otc.origin_system
+and iec.operation = otc.operation
 
 where order_to_cash.country = 'Brazil' -- Integração em paralelo por operação do país
 and order_to_cash.erp_subsidiary = 'BR010001' -- Filtro por filial (loop automático)
@@ -24,5 +57,5 @@ and order_to_cash.origin_system = 'corporatepass' -- Integração em paralelo po
 and order_to_cash.operation = 'revenue' -- Integração em paralelo por operação (plano de alunos, plano corporativo, etc...)
 and order_to_cash.to_generate_invoice = 'yes'
 and order_to_cash.erp_invoice_status_transaction = 'waiting_to_be_process' -- Filtrar somente os registros que ainda não foram integrados com o erp e estão aguardando processamento
-and invoice.erp_invoice_customer_id is not null -- Filtrar somente as invoices cujos os clientes já foram integrados anteriormente
+-- and invoice.erp_invoice_customer_id is not null -- Filtrar somente as invoices cujos os clientes já foram integrados anteriormente
 and invoice.erp_invoice_id is null -- Filtrar somente as invoices que ainda não foram integrados com o erp
