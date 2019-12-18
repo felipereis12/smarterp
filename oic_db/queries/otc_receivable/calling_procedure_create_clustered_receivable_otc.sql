77ec7a47fcf61a14101278c1d6640d99eee1ff62
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-12-18 18:55:33 -0300

    Update calling_procedure_create_clustered_receivable_otc.sql
     - Adição de chamada de proc para boletos

--- oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
+++ oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
@@ -1,3 +1,4 @@
 set sql_mode = traditional;
 call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_recurring');
 call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_account_recurring');
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','boleto');
