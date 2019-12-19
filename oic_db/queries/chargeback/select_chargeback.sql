36a8f0954b7305a35ed6573c1bf6944910c4aaca
felipe-nambara <felipe.nambara@bioritmo.com.br>
2019-12-19 18:34:09 -0300

    Update select_chargeback.sql
    Query ajustada para conter campos de integração com o Oracle (chargeback_erp_configurations)

--- oic_db/queries/chargeback/select_chargeback.sql
+++ oic_db/queries/chargeback/select_chargeback.sql
@@ -1,9 +1,40 @@
 select 
-	cchbk.* 
-    ,chbk.* 
-    ,crc.erp_customer_id
-    ,crc.full_name
+	 otc.erp_business_unit
+	,otc.erp_subsidiary
+	,otc.front_id
+	,otc.fin_id
+    ,otc.minifactu_id
+    ,otc.id
+	,cecg.erp_source_name
+	,cecg.erp_type_transaction
+    ,cecg.erp_payments_terms    
+    ,cecg.erp_currency_code
+    ,cecg.erp_currency_conversion_type
+    ,cecg.erp_interface_line_context
+    ,cecg.erp_payment_code
+    ,cecg.erp_set_of_books_id
+    ,cecg.erp_memo_line
+    ,cecg.erp_attribute_category     
+    ,cecg.warehouse_id
     ,crc.identification_financial_responsible
+    ,crc.full_name
+    ,chbk.erp_clustered_chargeback_id
+    ,chbk.gross_value 
+    ,chbk.conciliator_id
+    ,chbk.credit_card_brand
+    ,chbk.contract_number
+    ,chbk.transaction_type as chbk_transaction_type
+    ,chbk.truncated_credit_card
+    ,chbk.current_credit_card_installment
+    ,chbk.total_credit_card_installment
+    ,chbk.nsu
+    ,chbk.authorization_code
+    ,chbk.administration_tax_percentage
+    ,chbk.billing_date
+    ,chbk.credit_date
+    ,concat('RD_',rtrim(chbk.bank_number),'_',rtrim(chbk.bank_branch),'_',rtrim(chbk.bank_account)) as receipt_method
+    ,if(month(chbk.credit_date)=month(current_date()),chbk.credit_date,current_date()) as erp_trx_date
+    ,if(month(chbk.credit_date)=month(current_date()),chbk.credit_date,current_date()) as erp_gl_date    
 from chargeback chbk
 
 inner join clustered_chargeback cchbk
@@ -22,11 +53,17 @@ inner join order_to_cash otc
 on otc.country = chbk.country
 and otc.id = rec.order_to_cash_id
 
+left join chargeback_erp_configurations cecg
+on cecg.country = otc.country
+and cecg.origin_system = otc.origin_system
+and cecg.operation = otc.operation
+and cecg.transaction_type = rec.transaction_type
+
 where chbk.country = 'Brazil' -- Cada país deverá ter um processamento separado
 and otc.origin_system = 'smartsystem' -- Cada origem deverá ter um processamento separado
 and otc.operation = 'person_plan' -- Cada operação deverá ter um processamento separado
 and rec.transaction_type = 'credit_card_recurring' -- Cada tipo de transação deverá ter um processamento separado
 and chbk.erp_receipt_status_transaction = 'clustered_chargeback_created'
-and chbk.erp_clustered_chargeback_id is not null
+-- and chbk.erp_clustered_chargeback_id is not null
 and chbk.erp_receipt_id is null
 and chbk.concitiation_type = 'CHBK'; -- Considerar somente os retornos de comprovante de recebimento enviado pela conciliadora 
