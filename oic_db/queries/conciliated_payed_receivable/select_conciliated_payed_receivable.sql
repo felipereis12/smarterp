5834765b3a720ee637dc844b730698cdc5b67f88
felipereis12 <felipy_reis@hotmail.com>
2019-12-20 12:35:27 -0300

    Atualização 20/12/2019 v2
    Adicionados campos para mapeamento.

--- oic_db/queries/conciliated_payed_receivable/select_conciliated_payed_receivable.sql
+++ oic_db/queries/conciliated_payed_receivable/select_conciliated_payed_receivable.sql
@@ -1,25 +1,36 @@
 select 
 	otc.id
 	,otc.erp_business_unit
-    ,rec.erp_clustered_receivable_id
+    ,otc.erp_subsidiary
     ,crc.identification_financial_responsible
+    ,RTRIM (concat(identification_financial_responsible,'Faturar')) as Customer_Site 
     ,cpr.bank_number
     ,cpr.bank_branch
     ,cpr.bank_account
-    ,rec.net_value
+	,time (cpr.created_at) as created_hour
+    ,RTRIM (concat('RD_',cpr.bank_number,'_',cpr.bank_branch,'_',cpr.bank_account)) as Receipt_Method
     ,rec.conciliator_id
     ,rec.credit_card_brand
     ,rec.contract_number
     ,rec.transaction_type
-    ,rec.truncated_credit_card
-    ,rec.current_credit_card_installment
-    ,rec.total_credit_card_installment
-    ,rec.nsu
-    ,rec.authorization_code
-    ,rec.administration_tax_percentage
-    ,rec.administration_tax_value
-    ,rec.billing_date
     ,rec.credit_date
+    ,rec.credit_card_brand
+    ,RTRIM (concat('RD_',cpr.bank_number,'_',cpr.bank_branch,'_',cpr.bank_account,'_',rec.credit_date)) as Lote_Name
+    ,if (rec.transaction_type in ('credit_card_recurring','credit_card_tef','credit_card_pos','online_credit_card'), 'CARTOES DE CREDITO', 
+          if (rec.transaction_type = 'debit_account_recurring', 'DEPOSITO EM CONTA CORRENTE', 
+ 		     if (rec.transaction_type in ('debit_card_tef','debit_card_pos','online_debit_card'), 'CARTOES DE DEBITO', 
+			     if (rec.transaction_type = 'debit_card_recurring','DEBITO SEM SENHA', 
+                    if (rec.transaction_type = 'bank_transfer', 'TRANSFERENCIA BANCARIA', 
+                       if (rec.transaction_type = 'cash', 'DINHEIRO', 
+                          if (rec.transaction_type = 'boleto', 'BOLETO', null)
+                       )
+                    )
+                 )
+             )
+		)
+         
+	) as transaction_type
+    ,crc.full_name
 from conciliated_payed_receivable cpr
 
 inner join receivable rec
@@ -29,7 +40,7 @@ and rec.erp_clustered_receivable_customer_id is not null -- Considerar somente o
 and rec.converted_smartfin <> 'yes'
 
 inner join customer crc
-on crc.erp_customer_id = rec.erp_clustered_receivable_customer_id
+on crc.erp_customer_id = rec.erp_receivable_customer_id
 
 inner join order_to_cash otc
 on otc.country = cpr.country
@@ -42,4 +53,4 @@ and rec.transaction_type = 'credit_card_recurring' -- Cada tipo de transação d
 and rec.erp_receivable_id is not null
 and cpr.erp_receipt_status_transaction = 'waiting_to_be_process'
 and cpr.erp_receipt_id is null
-and cpr.concitiation_type = 'PCV'; -- Considerar somente os retornos de comprovante de recebimento enviado pela conciliadora 
+and cpr.concitiation_type = 'PCV'; -- Considerar somente os retornos de comprovante de recebimento enviado pela conciliadora
