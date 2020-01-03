8d1e433c45a4ddd599664c6d2aaf1eb5367fb722
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-03 18:01:12 -0300

    Update calling_procedure_create_clustered_receivable_otc.sql
     - Alteração da chamada da sp_create_clustered_receivable para passar como parâmetro a quantidade de receivables dentro de um clustered_receivable;

--- oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
+++ oic_db/queries/otc_receivable/calling_procedure_create_clustered_receivable_otc.sql
@@ -1,4 +1,7 @@
 set sql_mode = traditional;
-call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_recurring');
-call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_account_recurring');
-call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','boleto');
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','credit_card_recurring',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','debit_account_recurring',1500);
+call sp_create_clustered_receivable('Brazil','smartsystem','person_plan','boleto',1500);
+call sp_create_clustered_receivable('Brazil','biosystem','person_plan','credit_card_recurring',1500);
+call sp_create_clustered_receivable('Brazil','biosystem','person_plan','debit_account_recurring',1500);
+call sp_create_clustered_receivable('Brazil','biosystem','person_plan','boleto',1500);
