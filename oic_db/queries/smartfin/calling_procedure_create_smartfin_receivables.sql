058ac3ca890cbd93d94cc3ba590d83ccfd06c06d
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-03 18:05:17 -0300

    Update calling_procedure_create_smartfin_receivables.sql
     - Adição de chamadas de outras operações e outras marcas;

--- oic_db/queries/smartfin/calling_procedure_create_smartfin_receivables.sql
+++ oic_db/queries/smartfin/calling_procedure_create_smartfin_receivables.sql
@@ -1,3 +1,7 @@
 set sql_mode = traditional;
 call sp_create_smartfin_receivables('Brazil','smartsystem','person_plan','credit_card_recurring');
 call sp_create_smartfin_receivables('Brazil','smartsystem','person_plan','debit_account_recurring');
+call sp_create_smartfin_receivables('Brazil','smartsystem','person_plan','boleto');
+call sp_create_smartfin_receivables('Brazil','biosystem','person_plan','credit_card_recurring');
+call sp_create_smartfin_receivables('Brazil','biosystem','person_plan','debit_account_recurring');
+call sp_create_smartfin_receivables('Brazil','biosystem','person_plan','boleto');
