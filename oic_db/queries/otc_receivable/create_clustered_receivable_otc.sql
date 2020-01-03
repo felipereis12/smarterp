53656eb6c2c852d38774ed9946bc827f9525ac79
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-03 18:03:20 -0300

    Update create_clustered_receivable_otc.sql
     - Inclusão de parâmetro recebido que define o limite de receivables dentro de um clustered_receivable;

--- oic_db/queries/otc_receivable/create_clustered_receivable_otc.sql
+++ oic_db/queries/otc_receivable/create_clustered_receivable_otc.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS sp_create_clustered_receivable; 
 DELIMITER //
-CREATE PROCEDURE sp_create_clustered_receivable ( IN p_country varchar(45) , IN p_origin_system varchar(45) , IN p_operation varchar(45) , IN  p_transaction_type varchar(45) )
+CREATE PROCEDURE sp_create_clustered_receivable ( IN p_country varchar(45) , IN p_origin_system varchar(45) , IN p_operation varchar(45) , IN  p_transaction_type varchar(45), IN p_limit integer )
 
 BEGIN
 
@@ -11,7 +11,7 @@ declare v_erp_business_unit varchar(45);
 declare v_erp_legal_entity varchar(45);
 declare v_erp_subsidiary varchar(45);
 declare v_operation varchar(45);
-declare v_erp_customer_id int;
+declare v_erp_customer_id varchar(45);
 declare v_fullname varchar(255);
 declare v_transaction_type varchar(45);
 declare v_credit_card_brand varchar(45);
@@ -43,9 +43,11 @@ declare cur1 cursor for select * from vw_clustered_receivable
                         and transaction_type = p_transaction_type 
                         order by country
 								,origin_system
+                                ,unity_identification
                                 ,operation
                                 ,transaction_type
-                                ,billing_date;
+                                ,credit_card_brand
+                                ,billing_date /*limit 50*/;
 declare continue handler for not found set done=1;
 declare exit handler for sqlexception 
 begin
@@ -54,12 +56,36 @@ begin
     rollback; 
 end;
 
-set @v_keycontrol 	:= concat_ws('_','clus_rec',left(rtrim(p_country),2),left(rtrim(p_origin_system),2),left(rtrim(p_operation),2),rtrim(p_transaction_type));
+select concat("before: ",current_timestamp());
+
+set @v_keycontrol 	:= concat_ws('_','clus_rec',left(rtrim(p_country),2),left(rtrim(p_origin_system),2),left(rtrim(p_operation),2),left(rtrim(p_transaction_type),2));
 set @v_created_at	:= current_timestamp;
+
+-- select "teste";
+
+ -- select @v_keycontrol;
 
 if get_lock(@v_keycontrol,1) = 1 then 
 	
-
+    -- select "teste2";
+    
+    -- select p_country,p_origin_system,p_operation,p_transaction_type;
+    
+    /*
+	select * from vw_clustered_receivable 
+						where country = p_country 
+                        and origin_system = p_origin_system 
+                        and operation = p_operation
+                        and transaction_type = p_transaction_type 
+                        order by country
+								,origin_system
+                                ,operation
+                                ,transaction_type
+                                ,billing_date;    	
+	*/
+	
+    -- select "teste3";
+    
 	if ( select not exists(
 							select 
 								1 
@@ -69,13 +95,14 @@ if get_lock(@v_keycontrol,1) = 1 then
 							)
 		) then		
         
-        create table control_clustered_receivable (keycontrol varchar(150), created_at timestamp, id integer);        
+        CREATE TABLE control_clustered_receivable (  id int(11) NOT NULL AUTO_INCREMENT, keycontrol varchar(150) DEFAULT NULL,  created_at timestamp NULL DEFAULT NULL, PRIMARY KEY (id), KEY ccr_idx (keycontrol,created_at)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
     
     end if;
-
+    
     set done = 0;
-	
     open cur1;
+    
+    -- select "teste4";
     
     ClusteredReceivableLoop: loop
         fetch cur1 into  v_country
@@ -99,6 +126,7 @@ if get_lock(@v_keycontrol,1) = 1 then
 		if done = 1 then leave ClusteredReceivableLoop; end if;
 	      
         start transaction;
+        
 		/*
         select v_country
 						,v_origin_system
@@ -117,68 +145,72 @@ if get_lock(@v_keycontrol,1) = 1 then
                         ,v_billing_date
                         ,v_credit_date
                         ,v_is_smarftin;
-			*/
-        insert into control_clustered_receivable
+		*/
+		
+        -- select "teste5";
+			
+        
+        insert into control_clustered_receivable 
 								select 
-									 @v_keycontrol as keycontrol
+									order_to_cash.id as id
+									,@v_keycontrol as keycontrol
                                     ,@v_created_at as created_at
-									,order_to_cash.id as id
 								from order_to_cash
                                 
 								inner join receivable
 								on receivable.order_to_cash_id = order_to_cash.id
-                                                                
-								where receivable.erp_clustered_receivable_customer_id = v_erp_customer_id
-								and receivable.transaction_type = v_transaction_type
-								and receivable.contract_number = v_contract_number
-								and ( ( receivable.credit_card_brand is not null and receivable.credit_card_brand = v_credit_card_brand) or (receivable.credit_card_brand is null) )
-								and ( ( round(receivable.administration_tax_percentage,2) = round(v_administration_tax_percentage,2) ) or (receivable.administration_tax_percentage is null) )
-								and ( ( round(receivable.antecipation_tax_percentage,2) = round(v_antecipation_tax_percentage,2)) or (receivable.antecipation_tax_percentage is null) )
-								and receivable.billing_date = v_billing_date
-								and receivable.credit_date = v_credit_date
-								and receivable.is_smartfin = v_is_smarftin        
+								                                
+								where order_to_cash.country = v_country                                
 								and order_to_cash.origin_system = v_origin_system
 								and order_to_cash.operation = v_operation
 								and order_to_cash.unity_identification = v_unity_identification
-								and order_to_cash.erp_business_unit = v_erp_business_unit
-								and order_to_cash.erp_legal_entity = v_erp_legal_entity
-								and order_to_cash.erp_subsidiary = v_erp_subsidiary
-								and order_to_cash.country = v_country
 								and order_to_cash.erp_receivable_status_transaction = 'waiting_to_be_process'    
                                 and order_to_cash.to_generate_receivable = 'yes'
-                                /*
-                                and exists ( select 1 
-											from order_to_cash otc_v2  
-                                            
-                                            inner join invoice_customer invc
-                                            on invc.order_to_cash_id = otc_v2.id 
-                                            
-                                            where otc_v2.country = order_to_cash.country
-                                            and otc_v2.origin_system = order_to_cash.origin_system
-                                            and otc_v2.operation = order_to_cash.operation
-                                            and otc_v2.minifactu_id = order_to_cash.minifactu_id
-                                            and invc.erp_customer_id is not null )
-								*/
-								and not exists ( 
+                                and receivable.erp_clustered_receivable_customer_id = v_erp_customer_id
+								and receivable.transaction_type = v_transaction_type
+								and receivable.contract_number = v_contract_number
+								and ( ( receivable.credit_card_brand is not null and receivable.credit_card_brand = v_credit_card_brand) or (receivable.credit_card_brand is null) )
+								and receivable.billing_date = v_billing_date
+								and receivable.credit_date = v_credit_date
+								and receivable.is_smartfin = v_is_smarftin                                 
+       							and not exists ( 
 													select 
 														1 
 													from order_to_cash order_to_cash_v3
 													
 													inner join receivable receivable_v2
 													on receivable_v2.order_to_cash_id = order_to_cash_v3.id
-													
+													and	receivable_v2.erp_clustered_receivable_id is not null 
+
 													where order_to_cash_v3.unity_identification = order_to_cash.unity_identification
                                                     and order_to_cash_v3.minifactu_id =  order_to_cash.minifactu_id 
 													and ( 
 																( order_to_cash_v3.erp_receivable_status_transaction = 'clustered_receivable_being_created' ) 
-															or	( order_to_cash_v3.erp_receivable_status_transaction = 'clustered_receivable_created' ) 
-															or	( receivable_v2.erp_clustered_receivable_id is not null ) 
-															or	( receivable_v2.erp_receivable_id is not null ) 
+															or	( order_to_cash_v3.erp_receivable_status_transaction = 'clustered_receivable_created' ) 															
 														)
-												)  limit 300 ;
-                                               
-		if exists ( select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at)  then 
+												)  
+                                                
+								order by order_to_cash.country 
+										,order_to_cash.origin_system
+                                        ,order_to_cash.operation
+                                        ,order_to_cash.unity_identification
+										,order_to_cash.erp_receivable_status_transaction
+                                        ,order_to_cash.to_generate_receivable
+                                        
+                                        limit p_limit ;											
+		
+        -- select "teste6";
+		-- commit;
+		-- select "teste2";
         
+        -- select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at;
+        
+		if exists ( select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at order by id limit 1)  then 
+			
+            -- select "teste7";
+            
+            -- select count(1) from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at;
+            
 			update order_to_cash 
 			
 			inner join receivable
@@ -187,7 +219,9 @@ if get_lock(@v_keycontrol,1) = 1 then
 			set order_to_cash.erp_receivable_status_transaction = 'clustered_receivable_being_created'
 			
 			where order_to_cash.id in ( select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at);
-		
+			
+            -- select "teste8";
+            
 			select 	
 				 round(sum(receivable.gross_value),2) as gross_value
 				,round(sum(receivable.price_list_value),2) as price_list_value
@@ -213,6 +247,18 @@ if get_lock(@v_keycontrol,1) = 1 then
 			where order_to_cash.id in ( select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at)
 			and order_to_cash.erp_receivable_status_transaction = 'clustered_receivable_being_created' ;			
 			
+            -- select "teste9";
+            
+            /*
+            select @v_gross_value
+					,@v_price_list_value
+					,@v_net_value
+					,@v_interest_value
+					,@v_administration_tax_value
+					,@v_antecipation_tax_value
+					,@v_qtd_of_receivable;
+			*/
+            
 			insert into clustered_receivable
 								(country,
 								unity_identification,
@@ -253,7 +299,9 @@ if get_lock(@v_keycontrol,1) = 1 then
 								v_antecipation_tax_percentage,
 								@v_antecipation_tax_value,
 								@v_qtd_of_receivable);
-
+			
+            -- select "teste10";
+            
 			set @clustered_receivable_id = last_insert_id();
 			
 			update receivable
@@ -266,10 +314,14 @@ if get_lock(@v_keycontrol,1) = 1 then
 
 			where order_to_cash.id in ( select id from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at)
 			and order_to_cash.erp_receivable_status_transaction = 'clustered_receivable_being_created' ;        
+            
+            -- select "teste11";
         
         end if;
         
         delete from control_clustered_receivable where keycontrol = @v_keycontrol and created_at = @v_created_at;
+        
+        -- select "teste12";
         
     end loop ClusteredReceivableLoop;
     
@@ -280,6 +332,8 @@ if get_lock(@v_keycontrol,1) = 1 then
 else 
 	select concat('Procedure is already running in another thread: ',@v_keycontrol ) as log;
 end if;
+
+select concat("after: ",current_timestamp());
 
 END;
 //
