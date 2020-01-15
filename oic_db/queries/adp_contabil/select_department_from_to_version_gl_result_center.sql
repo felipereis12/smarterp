e7f885f8bc3280d0ab69fec9762f1ec0b44c327f
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-15 19:28:35 -0300

    Tratamento de selecionar o registro mais atual
    Tratamento de selecionar o registro mais atual

--- oic_db/queries/adp_contabil/select_department_from_to_version_gl_result_center.sql
+++ oic_db/queries/adp_contabil/select_department_from_to_version_gl_result_center.sql
@@ -4,4 +4,5 @@ erp_gl_result_center_code
 from department_from_to_version dftv
 
 where dftv.country = 'Brazil'
-and dftv.department_code = '<Colocar aqui a coluna DEPARTAMENTO do arquivo integrador contábil ADP>';
+and dftv.department_code = '<Colocar aqui a coluna DEPARTAMENTO do arquivo integrador contábil ADP>'
+and dftv.created_at = ( select max(dftv_v2.created_at) from department_from_to_version dftv_v2 where  dftv_v2.department_code = dftv.department_code ) ;
--- oic_db/queries/adp_contabil/select_natural_account_from_to_version_gl_natural_account.sql
+++ oic_db/queries/adp_contabil/select_natural_account_from_to_version_gl_natural_account.sql
@@ -4,4 +4,5 @@ erp_gl_natural_account_code
 from natural_account_from_to_version naftv
 
 where naftv.country = 'Brazil'
-and naftv.natural_account_code = '<Colocar aqui a coluna CT2_DEBITO (para lançamento a débito) ou CT2_CREDIT (para lançamento a crédito) do arquivo integrador contábil ADP>';
+and naftv.natural_account_code = '<Colocar aqui a coluna CT2_DEBITO (para lançamento a débito) ou CT2_CREDIT (para lançamento a crédito) do arquivo integrador contábil ADP>'
+and naftv.created_at = ( select max(naftv.created_at) from natural_account_from_to_version naftv_v2 where  naftv_v2.natural_account_code = naftv.natural_account_code ) ;
