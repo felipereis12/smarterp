00d8cb1fc50d4824ea537e6cdd92510b703be8e1
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-27 19:12:30 -0300

    Update calling_procedure_create_clustered_chageback.sql
    none

--- oic_db/queries/chargeback/calling_procedure_create_clustered_chageback.sql
+++ oic_db/queries/chargeback/calling_procedure_create_clustered_chageback.sql
@@ -1,3 +1,9 @@
 set sql_mode = traditional;
 call sp_create_clustered_chargeback('Brazil','smartsystem','person_plan','credit_card');
 call sp_create_clustered_chargeback('Brazil','smartsystem','person_plan','debit_card');
+call sp_create_clustered_chargeback('Brazil','biosystem','person_plan','credit_card');
+call sp_create_clustered_chargeback('Brazil','biosystem','person_plan','debit_card');
+call sp_create_clustered_chargeback('Brazil','racesystem','person_plan','credit_card');
+call sp_create_clustered_chargeback('Brazil','racesystem','person_plan','debit_card');
+call sp_create_clustered_chargeback('Brazil','nossystem','person_plan','credit_card');
+call sp_create_clustered_chargeback('Brazil','nossystem','person_plan','debit_card');
