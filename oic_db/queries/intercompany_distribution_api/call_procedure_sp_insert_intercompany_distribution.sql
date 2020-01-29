9424163d24fd9b17b4ac8bad5cf19bebc718a6f9
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-01-29 14:49:24 -0300

    Update call_procedure_sp_insert_intercompany_distribution.sql
    none

--- oic_db/queries/intercompany_distribution_api/call_procedure_sp_insert_intercompany_distribution.sql
+++ oic_db/queries/intercompany_distribution_api/call_procedure_sp_insert_intercompany_distribution.sql
@@ -6,7 +6,7 @@ set @json_request = cast(  '{
          "operation": "distribution",
          "unity_identification_receivable": 307,
          "unity_identification_payable": 15,
-         "front_intercompany_distribution_id": 1234,
+         "front_intercompany_distribution_id": 4234,
          "issue_date":"2019-12-30",
          "due_date":"2020-01-30",
          "gross_value": 12000
@@ -33,5 +33,3 @@ set @json_request = cast(  '{
 call sp_insert_inter_distrib( @json_request ,@p_return ,   @p_code ,@p_message, @p_inter_distrib_id); 
                             
 select @p_return as retorno ,@p_code,cast(@p_message as char),@p_inter_distrib_id;
-
--- Missing node otc.header.erp_receivable_customer_identification at Json request ! Check the documentation https://app.swaggerhub.com/apis-docs/Smartfit/OrderToCash/1.0.0
