bcdae2646bb835edd577ba1918969130cfba9457
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-02-10 16:12:02 -0300

    Update select_chargeback.sql
    Ajuste de quantidade de dígitos da agência bancária

--- oic_db/queries/chargeback/select_chargeback.sql
+++ oic_db/queries/chargeback/select_chargeback.sql
@@ -30,7 +30,7 @@ select
     ,chbk.billing_date
     ,chbk.credit_date
     ,convert(chbk.bank_account,unsigned) as bank_account
-    ,concat('RD_',rtrim(chbk.bank_number),'_',rtrim(chbk.bank_branch),'_',rtrim(convert(chbk.bank_account,unsigned))) as receipt_method
+    ,concat('RD_',rtrim(chbk.bank_number),'_',right(chbk.bank_branch,4),'_',rtrim(convert(chbk.bank_account,unsigned))) as receipt_method
     ,if(month(chbk.credit_date)=month(current_date()),chbk.credit_date,current_date()) as erp_trx_date
     ,if(month(chbk.credit_date)=month(current_date()),chbk.credit_date,current_date()) as erp_gl_date    
 from chargeback chbk
