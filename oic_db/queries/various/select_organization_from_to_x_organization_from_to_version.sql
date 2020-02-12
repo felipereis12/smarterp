804c4bbb8c00f55f33553b85a481c8f1bdeed7e0
felipe-nambara <felipe.nambara@bioritmo.com.br>
2020-02-12 14:17:46 -0300

    Update select_organization_from_to_x_organization_from_to_version.sql
     - Percorrer somente as filiais que estão ativas para a integração de customer, receivable ou invoice;

--- oic_db/queries/various/select_organization_from_to_x_organization_from_to_version.sql
+++ oic_db/queries/various/select_organization_from_to_x_organization_from_to_version.sql
@@ -1,15 +1,20 @@
 select 
-	 organization_from_to_version.erp_business_unit
-    ,organization_from_to_version.erp_legal_entity
-    ,organization_from_to_version.erp_subsidiary
-from organization_from_to
+	 oftv.erp_business_unit
+    ,oftv.erp_legal_entity
+    ,oftv.erp_subsidiary
+from organization_from_to oft
 
-inner join organization_from_to_version
-on organization_from_to_version.organization_from_to_unity_identification = organization_from_to.unity_identification
-and organization_from_to_version.created_at = ( 
+inner join organization_from_to_version oftv
+on oftv.organization_from_to_unity_identification = oft.unity_identification
+and oftv.created_at = ( 
 												select 
-													max(organization_from_to_version_v2.created_at) as created_at
-												from organization_from_to_version organization_from_to_version_v2 
-                                                where organization_from_to_version.organization_from_to_unity_identification = organization_from_to_version_v2.organization_from_to_unity_identification
+													max(oft_v2.created_at) as created_at
+												from organization_from_to_version oft_v2 
+                                                where oft_v2.organization_from_to_unity_identification = oftv.organization_from_to_unity_identification
 												)
+                                                
+where ( oftv.to_generate_customer = 'yes' 
+		or	oftv.to_generate_receivable = 'yes' 
+        or	oftv.to_generate_invoice = 'yes' )
+
 order by 1,2,3;
